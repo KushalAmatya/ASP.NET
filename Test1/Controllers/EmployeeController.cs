f02ae760ec2548c90966aa3f5d55581ca06992aa
kushal amatya <student3@sddb.student>
2022-03-10 08:42:48 +0545

    initial commit

---
+++ Test1/Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        // GET: Employee
+        public ActionResult EmpView()
+        {
+            return View();
+        }
+    }
+}
