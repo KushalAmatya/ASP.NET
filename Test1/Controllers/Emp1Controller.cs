9bf946059258d07f4cfbe472e1fdfbf43bdf72d8
kushal amatya <student3@sddb.student>
2022-03-13 09:27:18 +0545

    3rd day commit

--- Test1/Controllers/Emp1Controller.cs
+++ Test1/Controllers/Emp1Controller.cs
@@ -17,5 +17,6 @@ namespace Test1.Controllers
             return View(data);
          
         }
+     
     }
 }
--- Test1/Controllers/EmployeeController.cs
+++ Test1/Controllers/EmployeeController.cs
@@ -16,5 +16,16 @@ namespace Test1.Controllers
             List<tab_1> data = db.tab_1.ToList();
             return View(data);
         }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult SaveData(tab_1 tab_1)
+        {
+            db.tab_1.Add(tab_1);
+            db.SaveChanges();
+            return RedirectToAction("EmpView");
+                
+        }
     }
 }
