47d6417c3a324bf9b5676a41a52968be1334fadf
sandhyaphuyal27 <sfuyal@gmail.com>
2022-03-14 09:09:04 +0545

    4th day commit

--- Test1/Controllers/EmployeeController.cs
+++ Test1/Controllers/EmployeeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -26,6 +27,19 @@ namespace Test1.Controllers
             db.SaveChanges();
             return RedirectToAction("EmpView");
                 
+        }
+        public ActionResult Edit(int Id)
+        {
+            tab_1 data = db.tab_1.Find(Id);
+            //tab_1 data = db.tab_1.FirstOrDefault(x => x.Id == Id);
+            return View(data);
+        }
+        public ActionResult UpdateData(tab_1 tab_1)
+        {
+            db.Entry(tab_1).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("EmpView");
+
         }
     }
 }
