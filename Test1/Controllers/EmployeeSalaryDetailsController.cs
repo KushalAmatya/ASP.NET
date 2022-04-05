e314ce47ffcd37e1acf7c5bacd8506537ae49904
shikha12345 <shikhabhandari730@gmail.com>
2022-04-05 09:20:59 +0545

    7th day

--- Test1/Controllers/EmployeeSalaryDetailsController.cs
+++ Test1/Controllers/EmployeeSalaryDetailsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,15 +15,40 @@ namespace Test1.Controllers
         public ActionResult EmpSalView()
         {
             var emplyee_salary_details = db.employee_salary_details.ToList();
-            return View();
+            return View(emplyee_salary_details);
         }
-        public ActionResult Create()
+        public ActionResult Create1()
         {
+            var empList= db.employees.ToList();
+            ViewBag.employeeList = new SelectList(empList, "id", "name");
             return View();
         }
+
+        [HttpPost]
         public ActionResult SaveData(employee_salary_details employee_Salary_Details)
         {
             db.employee_salary_details.Add(employee_Salary_Details);
+            db.SaveChanges();
+           return RedirectToAction("EmpSalView");
+
+        }
+        public ActionResult EditSal(int Id)
+        {
+            employee_salary_details data = db.employee_salary_details.Find(Id);
+            //tab_1 data = db.tab_1.FirstOrDefault(x => x.Id == Id);
+            return View(data);
+        }
+        public ActionResult UpdateData(employee_salary_details employee_Salary_Details)
+        {
+            db.Entry(employee_Salary_Details).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("EmpSalView");
+
+        }
+        public ActionResult Delete(int Id)
+        {
+            employee_salary_details data = db.employee_salary_details.Find(Id);
+            db.employee_salary_details.Remove(data);
             db.SaveChanges();
             return RedirectToAction("EmpSalView");
 
